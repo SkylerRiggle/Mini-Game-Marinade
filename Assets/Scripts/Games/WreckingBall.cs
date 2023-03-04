3aa1b3eae9520f1ad455aad6e7f4e9f9869a03ca
Skyler Riggle <skyler32702@gmail.com>
2023-03-03 22:49:54 -0600

    Game Stubs - Skyler Riggle

---
+++ Assets/Scripts/Games/WreckingBall.cs
@@ -0,0 +1,25 @@
+// Title: WreckingBall
+// Author: Skyler Riggle
+
+public class WreckingBall : Game
+{
+    public void EndGame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Load()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void StartGame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UnLoad()
+    {
+        throw new System.NotImplementedException();
+    }
+}
