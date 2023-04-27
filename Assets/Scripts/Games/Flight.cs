5f04cecc9b7973ffd3a5007ea4a56ca3e4c88c9c
Eric Truong <erictruong10@ymail.com>
2023-04-27 16:57:28 -0500

    Created Flight branch

---
+++ Assets/Scripts/Games/Flight.cs
@@ -0,0 +1,39 @@
+// Title: Flight
+// Author: Eric Truong
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Control your flying player and hit the moving target!
+/// </summary>
+public class Flight : Game
+{
+    [SerializeField] private GameObject gameAssetParent = null;
+
+    public override int GetGameTime(int currentDifficulty)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void StartGame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool EndGame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Load()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UnLoad()
+    {
+        throw new System.NotImplementedException();
+    }
+}
